161c70bbe597fa6908201812248765d8ba8d7dab
Dimochka <dima.krokhaliov@yandex.ru>
2026-03-11 23:02:30 +0500

    -add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,22 @@ import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
 import { json } from 'express';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
   app.use(json());
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
