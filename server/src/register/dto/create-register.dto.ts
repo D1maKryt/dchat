9a9e9c31be5e7e58667b4eba6ff1907b735d9488
Dimochka <dima.krokhaliov@yandex.ru>
2026-03-14 17:49:54 +0500

    -edits

--- server/src/register/dto/create-register.dto.ts
+++ server/src/register/dto/create-register.dto.ts
@@ -1,29 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsDefined } from 'class-validator';
 
 export class CreateRegisterDto {
   @IsDefined()
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   username: string;
   @IsDefined()
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   password: string;
 }
 
 export class CreateRegisterAndServesDTO {
+  @ApiProperty()
   username: string;
+  @ApiProperty()
   password?: string;
+  @ApiProperty()
   accessToken?: string;
+  @ApiProperty()
   refreshToken?: string;
 }
 
 export class CreateServerDTO {
+  @ApiProperty()
   username: string;
+  @ApiProperty()
   accessToken: string;
+  @ApiProperty()
   refreshToken: string;
 }
 
 export class codeDTO {
+  @ApiProperty()
   code?: string;
 }
